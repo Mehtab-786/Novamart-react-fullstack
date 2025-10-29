b74f9ea1b1e5410092cc97a6e42404f9ea917c8c
Mehtab Hussain <tradercoder786@gmail.com>
2025-10-29 23:20:11 +0530

    axios set up

--- src/services/Axios.js
+++ src/services/Axios.js
@@ -1,12 +1,29 @@
 import axios from 'axios'
 
 const instance = axios.create({
-  baseURL: import.meta.env.BACKEND_API_URL,
-  timeout: 2000,
+  baseURL: import.meta.env.VITE_BACKEND_API_URL,
+  timeout: 8000,
   headers: {
     "Content-Type": 'application/json'
   },
   withCredentials: true
 });
+
+//Request intercepters
+instance.interceptors.request.use((config) => {
+  const token = localStorage.getItem('authToken');
+  if (token) config.headers.Authorization = `Bearer ${token}`;
+  return config;
+}, (err) => Promise.reject(err))
+
+//Response intercepters
+instance.interceptors.response.use((res) => res, async (err) => {
+  const { response } = err;
+  if (response?.status === 401) {
+    // Optionally trigger refresh or logout flow
+  }
+  return Promise.reject(err);
+});
+
 
 export default instance;
