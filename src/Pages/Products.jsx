4b348aa192c6cc687c5680baef6655b24a63dc84
Mehtab Hussain <tradercoder786@gmail.com>
2025-11-24 19:57:50 +0530

    fetching  products

--- src/Pages/Products.jsx
+++ src/Pages/Products.jsx
@@ -1,6 +1,16 @@
-import React from 'react'
+import { useEffect } from "react";
+import { useProduct } from "../Contexts/ProductContext"
+import { useNavigate } from "react-router-dom";
 
 function Products() {
+  const {allproduct} = useProduct();
+  const navigate = useNavigate()
+  useEffect(() => {
+    if(allproduct){
+      console.log(allproduct)
+    }
+  }, [navigate])
+
   return (
     <div>Products</div>
   )
--- src/main.jsx
+++ src/main.jsx
@@ -2,6 +2,7 @@ import { StrictMode } from "react";
 import { createRoot } from "react-dom/client";
 import "./index.css";
 import UserProvider from "./Contexts/AuthContext.jsx";
+import ProductProvider from "./Contexts/ProductContext.jsx";
 import App from "./App.jsx";
 import { BrowserRouter } from "react-router-dom";
 import { Toaster } from "sonner";
@@ -10,17 +11,19 @@ createRoot(document.getElementById("root")).render(
   <StrictMode>
     <BrowserRouter>
       <UserProvider>
-        <App />
-        <Toaster
-          position="top-right"
-          theme="system"
-          richColors
-          toastOptions={{
-            className:
-              "bg-gray-900 text-white rounded shadow-lg border border-gray-800 px-4 py-3 font-medium",
-            style: { fontSize: "1rem" },
-          }}
-        />
+        <ProductProvider>
+          <App />
+          <Toaster
+            position="top-right"
+            theme="system"
+            richColors
+            toastOptions={{
+              className:
+                "bg-gray-900 text-white rounded shadow-lg border border-gray-800 px-4 py-3 font-medium",
+              style: { fontSize: "1rem" },
+            }}
+          />
+        </ProductProvider>
       </UserProvider>
     </BrowserRouter>
   </StrictMode>
