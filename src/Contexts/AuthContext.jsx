7fef1aa5f9e89609875d7e4a1dbf65d0ad72e51d
Mehtab Hussain <tradercoder786@gmail.com>
2025-11-28 20:36:54 +0530

    authentication made

--- src/Contexts/AuthContext.jsx
+++ src/Contexts/AuthContext.jsx
@@ -1,5 +1,5 @@
-import { createContext, useContext, useState } from "react";
-import { loginUser, registerUser } from "../Services/Api";
+import { createContext, useContext, useEffect, useState } from "react";
+import { currentUser, loginUser, registerUser } from "../Services/Api";
 import { toast } from "sonner";
 import { useNavigate } from "react-router-dom";
 
@@ -10,19 +10,54 @@ export const useAuth = () => useContext(userContext);
 function UserProvider({ children }) {
   const [user, setUser] = useState();
   const [loading, setLoading] = useState(false);
-
   const navigate = useNavigate();
 
+
+  async function getCurrentuser() {
+    try {
+      setLoading(true);
+      const res = await currentUser();
+      console.log(res);
+      if(res.success == true){
+        setUser(res?.data);
+        toast.success("User authenticated");
+        console.log(res);
+      }
+    } catch (err) {
+      navigate('/login')
+      const errorMsg = err?.message || err?.error || "Login again";
+      toast.error(errorMsg);
+    } finally {
+      setLoading(false);
+    }
+  }
+
   async function newUserRegister(data) {
-    console.log(user)
     try {
       setLoading(true);
       const res = await registerUser(data);
-      console.log(res);
       if (res.success == true) {
         setUser(res?.data);
         toast.success("User registered succesfully");
         console.log(res);
+        navigate("/");
+      }
+    } catch (err) {
+      console.log(err)
+      const errorMsg = err?.message || err?.error || "Register failed";
+      toast.error(errorMsg);
+    } finally {
+      setLoading(false);
+    }
+  }
+
+  async function newUserLogin(data) {
+    try {
+      setLoading(true);
+      const res = await loginUser(data);
+      if (res.success == true) {
+        setUser(res?.data);
+        toast.success("User logged in succesfully");
         navigate("/");
       }
     } catch (err) {
@@ -34,24 +69,7 @@ function UserProvider({ children }) {
     }
   }
 
-  async function newUserLogin(data) {
-    console.log(user)
-    try {
-      setLoading(true);
-      const res = await loginUser(data);
-      console.log(res);
-      if (res.user) {
-        setUser(res.user);
-        toast.success("User logged in succesfully");
-        console.log(res);
-        navigate("/");
-      }
-    } catch (err) {
-      toast.error(err?.message);
-    } finally {
-      setLoading(false);
-    }
-  }
+
   
   return (
     <userContext.Provider value={{ user, newUserRegister, newUserLogin }}>
